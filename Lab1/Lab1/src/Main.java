db30f1280825c76008066a80664a3297028dc7d9
NeRpi <dmitrykruk12@gmail.com>
2023-10-09 11:43:40 +0300

    made task with zip

--- Lab1/Lab1/src/Main.java
+++ Lab1/Lab1/src/Main.java
@@ -6,7 +6,8 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Tasks Tasks = new Tasks();
-        System.out.println(Tasks.getCountWordsFromFile("D:\\University\\KPP\\Lab1\\Lab1\\src\\test.txt"));
-        System.out.println(Tasks.reverseArray(new ArrayList<Object>(Arrays.asList("строкак", 1, 3.12))) + "\nШвец А.М. " + new Date());
+        Tasks.createZip("src/");
+        System.out.println(Tasks.getCountWordsFromFile("src/test.txt"));
+        System.out.println(Tasks.reverseArray(new ArrayList<>(Arrays.asList("строкак", 1, 3.12))) + "\nШвец А.М. " + new Date());
     }
 }
--- Lab1/Lab1/src/Tasks.java
+++ Lab1/Lab1/src/Tasks.java
@@ -1,10 +1,24 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Tasks {
+    void createZip(String dirPath) {
+        File dir = new File(dirPath);
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(dirPath + "output.zip"));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(zout))) {
+            zout.putNextEntry(new ZipEntry("resultFile.txt"));
+            for (File fileTXT : Objects.requireNonNull(dir.listFiles((file, name) -> name.endsWith(".txt"))))
+                writer.write(Files.readString(fileTXT.toPath()));
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
     int getCountWordsFromFile(String fileName) {
         int countWords = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
@@ -17,6 +31,7 @@ public class Tasks {
         }
         return countWords;
     }
+
     ArrayList<Object> reverseArray(ArrayList<Object> arr) {
         ArrayList<Object> newArr = new ArrayList<>(arr);
         Collections.reverse(newArr);
